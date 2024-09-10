387dd9742923af18d2c9362c7ae89887e89aaded
sumedh-21 <156858057+sumedh-21@users.noreply.github.com>
2024-09-09 20:11:55 -0400

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Hero from "./sections/Hero/Hero";
-import Navbar from "./sections/Navbar/navbar";
+import Navbar from "./sections/Navbar/Navbar";
 import Skills from "./sections/Skills/Skills";
 import Projects from "./sections/Projects/Projects";
 import Contact from "./sections/Contact/Contact";
