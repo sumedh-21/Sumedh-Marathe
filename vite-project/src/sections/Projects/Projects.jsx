23d951e5882ebff719cf9f06c01df050b572619c
sumedh-21 <156858057+sumedh-21@users.noreply.github.com>
2024-11-06 01:11:21 -0500

    updates

--- vite-project/src/sections/Projects/Projects.jsx
+++ vite-project/src/sections/Projects/Projects.jsx
@@ -3,26 +3,23 @@ import styles from "./ProjectsStyles.module.css";
 function Projects() {
   const projects = [
     {
-      name: "SlangScribe: Transcribe,Translate and Genzify Your Lectures",
-      description: `SlangScribe is a web application designed to transcribe audio files, 
-      summarize the transcriptions, translate them into multiple languages, and even "Genzify" 
-      the content into Gen-Z slang for added fun! It uses OpenAI's GPT-4 for high-quality text generation and summarization, 
-      Whisper for transcription, and Hugging Face pipelines for translation.`,
+      name: "ClearBill: AI-Driven Healthcare Billing Optimization System",
+      link: "https://github.com/joelkny97/ruhealth2024-team7",
+    },
+    {
+      name: "SlangScribe: Transcribe, Translate and Genzify Your Lectures",
       link: "https://github.com/sumedh-21/SlangScribe-Translate-Transcribe-and-Genzify-your-Lectures",
     },
     {
       name: "Incremental Shortest Path Algorithm for Dynamic Network Optimization",
-      description: `Developed an Incremental Shortest Path Algorithm that adapts to dynamic network changes, offering up to 50x faster performance compared to traditional methods.`,
       link: "https://github.com/sumedh-21/ECE-573-DSA-finalproject",
     },
     {
       name: "Pruning a LeNet-5 Convolutional Neural Network on MNIST Dataset",
-      description: `Reduced the size of a LeNet-5 model by 51% using L1 Unstructured Pruning while maintaining high accuracy.`,
       link: "https://github.com/sumedh-21/Pruning-a-LeNet-5-Convolutional-Neural-Network-on-MNIST-Dataset-",
     },
     {
       name: "Comparative Analysis of Methods To Alleviate CPU-GPU Data Transfer Overhead",
-      description: `Enhanced CPU-GPU data transfer in CUDA using Unified Memory and Thread Hierarchy, improving performance and energy efficiency.`,
       link: "https://github.com/sumedh-21/cuda-memoryops",
     },
   ];
@@ -38,7 +35,6 @@ function Projects() {
                 {project.name}
               </a>
             </h3>
-            <p className={styles.description}>{project.description}</p>
           </div>
         ))}
       </div>
--- vite-project/src/sections/Skills/Skills.jsx
+++ vite-project/src/sections/Skills/Skills.jsx
@@ -16,9 +16,12 @@ function Skills() {
         <SkillList src={checkMarkIcon} skill="Python" />
         <SkillList src={checkMarkIcon} skill="C++" />
         <SkillList src={checkMarkIcon} skill="C" />
+        <SkillList src={checkMarkIcon} skill="Java" />
+        <SkillList src={checkMarkIcon} skill="JavaScript" />
+        <SkillList src={checkMarkIcon} skill="SQL" />
         <SkillList src={checkMarkIcon} skill="HTML" />
         <SkillList src={checkMarkIcon} skill="CSS" />
-        <SkillList src={checkMarkIcon} skill="JavaScript" />
+        <SkillList src={checkMarkIcon} skill="MATLAB" />
         <SkillList src={checkMarkIcon} skill="CUDA" />
         <SkillList src={checkMarkIcon} skill="REST API" />
         <SkillList src={checkMarkIcon} skill="NLP" />
@@ -26,26 +29,33 @@ function Skills() {
       <hr />
       <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="React" />
-        <SkillList src={checkMarkIcon} skill="SQL" />
-        <SkillList src={checkMarkIcon} skill="PyTorch" />
-        <SkillList src={checkMarkIcon} skill="PySpark" />
-        <SkillList src={checkMarkIcon} skill="Numpy" />
-        <SkillList src={checkMarkIcon} skill="TensorFlow" />
-        <SkillList src={checkMarkIcon} skill="Github" />
-        <SkillList src={checkMarkIcon} skill="Generative AI" />
-        <SkillList src={checkMarkIcon} skill="Sci-kit-learn" />
+        <SkillList src={checkMarkIcon} skill="Flask" />
+        <SkillList src={checkMarkIcon} skill="Next.js" />
+        <SkillList src={checkMarkIcon} skill="Node.js" />
       </div>
       <hr />
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="Matplotlib" />
-        <SkillList src={checkMarkIcon} skill="Pandas" />
-        <SkillList src={checkMarkIcon} skill="VmWare" />
-        <SkillList src={checkMarkIcon} skill="Hadoop" />
-        <SkillList src={checkMarkIcon} skill="SimpleScalar" />
-        <SkillList src={checkMarkIcon} skill="WireShark" />
         <SkillList src={checkMarkIcon} skill="Docker" />
-        <SkillList src={checkMarkIcon} skill="AutoCad" />
-        <SkillList src={checkMarkIcon} skill="Git/Github" />
+        <SkillList src={checkMarkIcon} skill="Git/GitHub" />
+        <SkillList src={checkMarkIcon} skill="VmWare" />
+        <SkillList src={checkMarkIcon} skill="AWS" />
+        <SkillList src={checkMarkIcon} skill="Hadoop" />
+        <SkillList src={checkMarkIcon} skill="OpenMP" />
+        <SkillList src={checkMarkIcon} skill="MPI" />
+        <SkillList src={checkMarkIcon} skill="AVX Intrinsics" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="PyTorch" />
+        <SkillList src={checkMarkIcon} skill="TensorFlow" />
+        <SkillList src={checkMarkIcon} skill="Hugging Face" />
+        <SkillList src={checkMarkIcon} skill="Sci-kit Learn" />
+        <SkillList src={checkMarkIcon} skill="NumPy" />
+        <SkillList src={checkMarkIcon} skill="Pandas" />
+        <SkillList src={checkMarkIcon} skill="Matplotlib" />
+        <SkillList src={checkMarkIcon} skill="Whisper" />
+        <SkillList src={checkMarkIcon} skill="Generative AI" />
+        <SkillList src={checkMarkIcon} skill="CNN" />
       </div>
     </section>
   );
